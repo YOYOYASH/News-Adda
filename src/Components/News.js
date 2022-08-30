18e5aad44f644b69e6b06e46b9a12c386feea541
Yash Mishra <ym911837@student.nitw.ac.in>
2022-08-30 22:53:06 +0530

    Next And Previous bttons added

--- src/Components/News.js
+++ src/Components/News.js
@@ -2,24 +2,55 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export class News extends Component {
-  
   constructor() {
     super();
-   this.state={
-    articles:[]
-   }
+    this.state = {
+      articles: [],
+      page: 1,
+    };
   }
-  async componentDidMount(){
-   let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=cfbf3f2a6e894c929ceefd3dc60299af";
-   let data=await fetch(url);
-   let parsedData= await  data.json();
-   console.log(parsedData);
-   this.setState({
-     articles:parsedData.articles,
-   })
-  
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=cfbf3f2a6e894c929ceefd3dc60299af";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+    });
+  } //loads after render
 
-  }//loads after render
+  handlePreviousClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=cfbf3f2a6e894c929ceefd3dc60299af&page=${
+      this.state.page - 1
+    }&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      articles: parsedData.articles,
+      page: this.state.page - 1,
+    });
+  };
+
+  handleNextClick = async () => {
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=cfbf3f2a6e894c929ceefd3dc60299af&page=${
+        this.state.page + 1
+      }&pageSize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData);
+      this.setState({
+        articles: parsedData.articles,
+
+        page: this.state.page + 1,
+      });
+    }
+  };
+
   render() {
     return (
       <div className="container my-3">
@@ -38,6 +69,23 @@ export class News extends Component {
               </div>
             );
           })}
+        </div>
+        <div className="d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlePreviousClick}
+          >
+            &larr; Previous
+          </button>
+          <button
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
